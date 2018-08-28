e35f39bc073ed3078666004db56324a53353dc93
bsevgeny@gmail.com <papakita2009>
2018-08-28 17:49:44 +0300

    spring

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -1,7 +1,11 @@
 package quoters;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -9,8 +13,10 @@ import java.util.Map;
  * @author Evgeny Borisov
  */
 public class Main {
+
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
      /*   String[] names = context.getBeanDefinitionNames();
         Arrays.stream(names).forEach(System.out::println);*/
     }
